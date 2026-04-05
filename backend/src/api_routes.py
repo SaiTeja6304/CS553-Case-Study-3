4ada6871c55f608f22fb707740e00d268471b875
irpatel0 <irpatel@wpi.edu>
2026-04-05 17:31:22 -0400

    Promethus metrics

--- backend/src/api_routes.py
+++ backend/src/api_routes.py
@@ -1,38 +1,97 @@
-from fastapi import APIRouter, HTTPException, File, UploadFile, Form
+from fastapi import APIRouter, HTTPException, File, UploadFile, Form, Response
 from .services import generate_response_api, generate_response_local
+from time import perf_counter
+import prometheus_client
 
 router = APIRouter( 
    prefix="/api",  
    tags=["VLM Chat API"]  
 ) 
 
+API_BACKEND_REQUESTS_TOTAL = prometheus_client.Counter(
+    'api_backend_requests_total',
+    'Total number of API requests made.'
+)
+
+LOCAL_BACKEND_REQUESTS_TOTAL = prometheus_client.Counter(
+    'local_backend_requests_total',
+    'Total number of local requests made.'
+)
+
+API_BACKEND_ERRORS_TOTAL = prometheus_client.Counter(
+    'api_backend_errors_total',
+    'Total number of API request errors occured in backend.'
+)
+
+LOCAL_BACKEND_ERRORS_TOTAL = prometheus_client.Counter(
+    'local_backend_errors_total',
+    'Total number of local request errors occured in backend.'
+)
+
+API_BACKEND_REQUESTTIME_SECONDS = prometheus_client.Histogram(
+    'api_backend_requesttime_seconds',
+    'Total time spent processing backend API requests.'
+)
+
+LOCAL_BACKEND_REQUESTTIME_SECONDS = prometheus_client.Histogram(
+    'local_backend_requesttime_seconds',
+    'Total time spent processing backend local requests.'
+)
+
+HEALTH_CHECK_REQUESTS_TOTAL = prometheus_client.Counter(
+    'health_check_requests_total',
+    'Total number of health check requests.'
+)
+
 @router.get("/", status_code=200, response_model=dict)
 async def root():
+    HEALTH_CHECK_REQUESTS_TOTAL.inc()
     """Root endpoint for the VLM Chat API"""
     return {"message": "VLM Chat API", "status": "running"}
+
+@router.get('/metrics', status_code=200)
+async def metrics():
+    return Response(
+        content = prometheus_client.generate_latest(),
+        media_type= prometheus_client.CONTENT_TYPE_LATEST
+    )
 
 @router.post("/generate-response-local", status_code=200, response_model=dict)
 async def generate_response_local_route(input_img: UploadFile = File(...), query: str = Form(...), chat_history: str = Form("")):
     """Generate response using local model"""
+    LOCAL_BACKEND_REQUESTS_TOTAL.inc()
+    local_start_time = perf_counter()
     try:
         result = await generate_response_local(input_img, query, True, chat_history)
         if result["db_insert"] != "Successfully inserted log":
             raise HTTPException(status_code=500, detail=result["db_insert"])
         return result
     except HTTPException:
+        LOCAL_BACKEND_ERRORS_TOTAL.inc()
         raise
     except Exception as e:
+        LOCAL_BACKEND_ERRORS_TOTAL.inc()
         raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        LOCAL_BACKEND_REQUESTTIME_SECONDS.observe(perf_counter() - local_start_time)
+
 
 @router.post("/generate-response-api", status_code=200, response_model=dict)
 async def generate_response_api_route(input_img: UploadFile = File(...), query: str = Form(...), chat_history: str = Form("")):
     """Generate response using HF Inference API"""
+    API_BACKEND_REQUESTS_TOTAL.inc()
+    api_start_time = perf_counter()
     try:
         result = await generate_response_api(input_img, query, False, chat_history)
         if result["db_insert"] != "Successfully inserted log":
             raise HTTPException(status_code=500, detail=result["db_insert"])
         return result
     except HTTPException:
+        API_BACKEND_ERRORS_TOTAL.inc()
         raise
     except Exception as e:
+        API_BACKEND_ERRORS_TOTAL.inc()
         raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        API_BACKEND_REQUESTTIME_SECONDS.observe(perf_counter() - api_start_time)
+
--- frontend/src/streamlit_app.py
+++ frontend/src/streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
 import requests
 import prometheus_client
+from time import perf_counter
+
+API_FRONTEND_REQUESTS_TOTAL = prometheus_client.Counter(
+    'api_frontend_requests_total',
+    'Total number of API requests made.'
+)
+
+LOCAL_FRONTEND_REQUESTS_TOTAL = prometheus_client.Counter(
+    'local_frontend_requests_total',
+    'Total number of local requests made.'
+)
+
+API_FRONTEND_ERRORS_TOTAL = prometheus_client.Counter(
+    'api_frontend_errors_total',
+    'Total number of API request errors occured in backend.'
+)
+
+LOCAL_FRONTEND_ERRORS_TOTAL = prometheus_client.Counter(
+    'local_frontend_errors_total',
+    'Total number of local request errors occured in backend.'
+)
+
+API_FRONTEND_REQUESTTIME_SECONDS = prometheus_client.Histogram(
+    'api_frontend_requesttime_seconds',
+    'Total time spent processing backend API requests.'
+)
+
+LOCAL_FRONTEND_REQUESTTIME_SECONDS = prometheus_client.Histogram(
+    'local_frontend_requesttime_seconds',
+    'Total time spent processing backend local requests.'
+)
+
 
 st.set_page_config(page_title="VLM Chat", page_icon=":robot_face:", layout="wide")
 st.title("VLM Chat")
@@ -33,7 +65,6 @@ with st.sidebar:
         except Exception as e:
             st.error(f"VLM Chat API is not running: {e}", icon="❌")
         
-
 image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 st.session_state.uploaded_image = image
 if image is not None:
@@ -44,6 +75,7 @@ if query:
     if st.session_state.uploaded_image is None:
         st.error("Please upload an image")
     else:
+        req_start_time = perf_counter()
         st.chat_message("user").markdown(query)
 
         with st.spinner("Processing..."):
@@ -51,10 +83,12 @@ if query:
             chat_history_str = "\n".join([f"{role}: {msg}" for role, msg in st.session_state.chat_history])
             
             if use_local_model:
+                LOCAL_FRONTEND_REQUESTS_TOTAL.inc()
                 response = requests.post(API_SERVER + "/api/generate-response-local", 
                 files={"input_img": image}, 
                 data={"query": query, "chat_history": chat_history_str}, timeout=HTTP_MAX_TIMEOUT_SECONDS)
             else:
+                API_FRONTEND_REQUESTS_TOTAL.inc()
                 response = requests.post(API_SERVER + "/api/generate-response-api", 
                 files={"input_img": image}, 
                 data={"query": query, "chat_history": chat_history_str}, timeout=HTTP_MAX_TIMEOUT_SECONDS)
@@ -64,7 +98,24 @@ if query:
             st.session_state.chat_history.append(("user", query))
             st.session_state.chat_history.append(("assistant", response.json()["response"]))
         else:
+            if use_local_model:
+                LOCAL_FRONTEND_ERRORS_TOTAL.inc()
+            else:
+                API_FRONTEND_ERRORS_TOTAL.inc()
             st.error(response.json().get("detail", "Unknown error"))
+        
+        if use_local_model:
+            LOCAL_FRONTEND_REQUESTTIME_SECONDS.observe(perf_counter() - req_start_time)
+        else:
+            API_FRONTEND_REQUESTTIME_SECONDS.observe(perf_counter() - req_start_time)
+        
 
-prometheus_client.start_http_server(22013)
+@st.cache_resource
+def start_server():
+    prometheus_client.start_http_server(22013)
+    return
+
+start_server()
+
+
 # streamlit run frontend/src/streamlit_app.py --server.port 7011 --server.address 0.0.0.0
